5b1305cf43a336f79eea902bc06f408a7ffebbe5
xiaomagexiao <mayanjunchina@163.com>
2017-04-25 22:00:10 +0800

    完善项目

--- search_web/src/com/main/servlet/CmdUtils.java
+++ search_web/src/com/main/servlet/CmdUtils.java
@@ -1,10 +1,7 @@
 package com.main.servlet;
 
 import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.nio.charset.Charset;
 
 public class CmdUtils {
 
--- search_web/src/com/main/servlet/SearchServlet.java
+++ search_web/src/com/main/servlet/SearchServlet.java
@@ -1,11 +1,12 @@
 package com.main.servlet;
 
 
+import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 /**
  * Created by mama on 2017/4/23.
